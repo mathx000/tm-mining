0ef56cf4304df838adccebc566fd53c7bcb2abac
mathx000 <matheusgmc98@gmail.com>
2026-08-08 17:01:45 +0100

    Add machine image imports mapping
    - Created images.ts for mapping machine names to imported images
    - Ready for integration into equipment detail component
    - Supports 12 new machines from machines folder

---
+++ frontend/src/machines/images.ts
@@ -0,0 +1,26 @@
+// Image imports for equipment catalog
+import allisChalmerImg from '../img/Britadeira de maxilas Allis-Chalmers 1100 x 600.jpg';
+import metsoC125Img from '../img/Britadeira de maxilas Metso Nordberg C125.jpg';
+import parkerImg from '../img/Britadeira de maxilas Parker 36X24.jpg';
+import sanyImg from '../img/Britadeira de maxilas Sany SANYO 1050 x 750.jpg';
+import metsoHp200Img from '../img/Britador de cone Metso HP200.jpg';
+import sandvikCh430Img from '../img/Britador de cone Sandvik CH430.jpg';
+import sandvikCs430Img from '../img/Britador de cone Sandvik cs 430.jpg';
+import komatsuImg from '../img/Escavadora de rastos Komatsu PC600.jpg';
+import kubotaImg from '../img/Mini-escavadora Kubota KX057-4.jpg';
+import yanmarImg from '../img/Mini-escavadora Yanmar Vio 55.jpg';
+import metsoBarmaB6Img from '../img/Trituradora de rotor Metso BARMAC B6150SE VSI.jpg';
+
+export const machineImages = {
+  'Britadeira de maxilas Allis-Chalmers 1100 x 600': allisChalmerImg,
+  'Britadeira de maxilas Metso Nordberg C125': metsoC125Img,
+  'Britadeira de maxilas Parker 36X24': parkerImg,
+  'Britadeira de maxilas Sany SANYO 1050 x 750': sanyImg,
+  'Britador de cone Metso HP200': metsoHp200Img,
+  'Britador de cone Sandvik CH430': sandvikCh430Img,
+  'Britador de cone Sandvik cs 430': sandvikCs430Img,
+  'Escavadora de rastos Komatsu PC600': komatsuImg,
+  'Mini-escavadora Kubota KX057-4': kubotaImg,
+  'Mini-escavadora Yammer Vio 55': yanmarImg,
+  'Trituradora de rotor Metso BARMAC B6150SE VSI': metsoBarmaB6Img
+};
